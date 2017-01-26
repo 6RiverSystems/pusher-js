de27c454c4cfc1cbbda7a0acd22ef6c3f168f7d1
Tony Gaetani <tony.gaetani@gmail.com>
2017-01-26 12:39:01 -0600

    updating compiled js

--- dist/node/pusher.js
+++ dist/node/pusher.js
@@ -2308,28 +2308,14 @@ module.exports =
 	    }
 	};
 	exports.getCloseAction = function (closeEvent) {
-	    if (closeEvent.code < 4000) {
-	        if (closeEvent.code >= 1002 && closeEvent.code <= 1004) {
-	            return "backoff";
-	        }
-	        else {
-	            return null;
-	        }
-	    }
-	    else if (closeEvent.code === 4000) {
+	    if (closeEvent.code === 4000) {
 	        return "ssl_only";
 	    }
-	    else if (closeEvent.code < 4100) {
-	        return "refused";
-	    }
-	    else if (closeEvent.code < 4200) {
-	        return "backoff";
-	    }
-	    else if (closeEvent.code < 4300) {
+	    else if (closeEvent.code >= 4200 && closeEvent.code < 4300) {
 	        return "retry";
 	    }
 	    else {
-	        return "refused";
+	        return "backoff";
 	    }
 	};
 	exports.getCloseError = function (closeEvent) {
